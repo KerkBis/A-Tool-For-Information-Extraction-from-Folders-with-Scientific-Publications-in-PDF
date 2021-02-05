c461ce70d4513dc61e28e47f6f07a907feb420cf
Kerk <kerkbisiklis@gmail.com>
2021-02-05 21:42:58 +0200

    Refactored code and added DocumentEditor
    Document Reader was removed and functionality transfered to DocumnetEditor

--- ToolToManagePdfs/src/main/java/application/App.java
+++ ToolToManagePdfs/src/main/java/application/App.java
@@ -10,9 +10,9 @@ public class App
     {
         //DocumentCreation creator = new DocumentCreation();
         //creator.makeDoc("C:/Users/tamag/Desktop", "my_doc.pdf", "Tamag", "DocumentCreationTest");
-        DocumentReader reader = new DocumentReader("C:/Users/tamag/Desktop/sample.pdf");
+        DocumentEditor editor = new DocumentEditor("C:/Users/tamag/Desktop/sample.pdf");
 
-        System.out.println("Some of the properties are: " + reader.getAuthor());
-        System.out.println(reader.getScannedText());
+        System.out.println("Some of the properties are: " + editor.getAuthor());
+        System.out.println(editor.getScannedText());
     }
 }
--- ToolToManagePdfs/src/main/java/application/DocumentEditor.java
+++ ToolToManagePdfs/src/main/java/application/DocumentEditor.java
@@ -8,8 +8,6 @@ package application;
 import java.io.File;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -20,28 +18,14 @@ import org.apache.pdfbox.text.PDFTextStripper;
  *
  * @author tamag
  */
-public class DocumentReader {
+public class DocumentEditor {
 
     private PDDocument doc;
     private String path, name, author, creator, title, subject, keywords;
     private Calendar creationDate, modifiedDate;
     private String scannedText;
 
-    public DocumentReader() {
-        this.doc = null;
-        this.path = null;
-        this.name = null;
-        this.author = null;
-        this.creator = null;
-        this.title = null;
-        this.subject = null;
-        this.creationDate = null;
-        this.modifiedDate = null;
-        this.keywords = null;
-        this.scannedText = null;
-    }
-
-    public DocumentReader(String pathname) {
+    public DocumentEditor(String pathname) {
 
         try {
             //get the pdf file to read it
@@ -65,9 +49,8 @@ public class DocumentReader {
             this.keywords = docInformation.getKeywords();
 
         } catch (IOException ex) {
-            Logger.getLogger(DocumentReader.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DocumentEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
     public void readFile(String pathname) {
@@ -93,68 +76,109 @@ public class DocumentReader {
             this.keywords = docInformation.getKeywords();
 
         } catch (IOException ex) {
-            Logger.getLogger(DocumentReader.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DocumentEditor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public List getPropertiesList() {
-
-        List properiesList = new LinkedList<>();
-
-        properiesList.add(name);
-        properiesList.add(author);
-        properiesList.add(creator);
-        properiesList.add(title);
-        properiesList.add(subject);
-        properiesList.add(creationDate);
-        properiesList.add(modifiedDate);
-        properiesList.add(keywords);
-
-        return properiesList;
+    public void editProperties(String property, String value) {
+        switch (property) {
+            case "name":
+                setName(value);
+                break;
+            case "author":
+                setAuthor(value);
+                break;
+            case "title":
+                setTitle(value);
+        }
     }
 
-    public PDDocument getDoc() {
-        return doc;
+    public DocumentReader getReader() {
+        return reader;
+    }
+
+    public void setReader(DocumentReader reader) {
+        this.reader = reader;
     }
 
     public String getPath() {
         return path;
     }
 
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getCreator() {
         return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getSubject() {
         return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
     public String getKeywords() {
         return keywords;
     }
 
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
     public Calendar getCreationDate() {
         return creationDate;
+    }
+
+    public void setCreationDate(Calendar creationDate) {
+        this.creationDate = creationDate;
     }
 
     public Calendar getModifiedDate() {
         return modifiedDate;
     }
 
+    public void setModifiedDate(Calendar modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
     public String getScannedText() {
         return scannedText;
+    }
+
+    public void setScannedText(String scannedText) {
+        this.scannedText = scannedText;
     }
 
 }
