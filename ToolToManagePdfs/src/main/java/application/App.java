c7a8bdfd328655aba7add82b9961d029470a8c09
Kerk <kerkbisiklis@gmail.com>
2021-02-05 21:20:51 +0200

    Renamed project and simplyfied structure

--- ToolToManagePdfs/src/main/java/application/App.java
+++ ToolToManagePdfs/src/main/java/application/App.java
@@ -1,6 +1,4 @@
-package com.mycompany.app;
-
-import java.io.IOException;
+package application;
 
 /**
  * Hello world!
@@ -8,7 +6,8 @@ import java.io.IOException;
  */
 public class App 
 {
-    public static void main(String[] args) throws IOException {
+    public static void main( String[] args )
+    {
         //DocumentCreation creator = new DocumentCreation();
         //creator.makeDoc("C:/Users/tamag/Desktop", "my_doc.pdf", "Tamag", "DocumentCreationTest");
         DocumentReader reader = new DocumentReader("C:/Users/tamag/Desktop/sample.pdf");
--- ToolToManagePdfs/src/test/java/application/AppTest.java
+++ ToolToManagePdfs/src/test/java/application/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package application;
 
 import static org.junit.Assert.assertTrue;
 
